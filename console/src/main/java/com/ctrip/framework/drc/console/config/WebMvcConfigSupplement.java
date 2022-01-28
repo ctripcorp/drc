ed154b660bbcc41e42c81f43610f6efd2d27565b
hdpan <hdpan@trip.com>
2022-01-28 17:07:58 +0800

    sso_test2

--- console/src/main/java/com/ctrip/framework/drc/console/config/WebMvcConfigSupplement.java
+++ console/src/main/java/com/ctrip/framework/drc/console/config/WebMvcConfigSupplement.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.*;
 
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -22,7 +24,14 @@ import java.util.logging.Logger;
 @Configuration
 public class WebMvcConfigSupplement extends WebMvcConfigurerAdapter {
     private final Logger logger = Logger.getLogger(getClass().getSimpleName());
-    
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
+    }
+
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         super.extendMessageConverters(converters);
@@ -36,8 +45,9 @@ public class WebMvcConfigSupplement extends WebMvcConfigurerAdapter {
         if (defaultConverter != null) {
             converters.remove(defaultConverter);
             converters.set(0, defaultConverter);
+            logger.info("!!! default converter use MappingJackson2HttpMessageConverter");
         }
-        logger.info("!!! default converter use MappingJackson2HttpMessageConverter");
+        logger.info("!!! default converter no use MappingJackson2HttpMessageConverter");
     }
-    
+
 }
