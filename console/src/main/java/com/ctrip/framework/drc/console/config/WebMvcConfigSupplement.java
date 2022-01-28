44818d074d8b1f70b939e42c6d1b6f20143ab898
hdpan <hdpan@trip.com>
2022-01-28 15:20:58 +0800

    sso_upgrade_test

---
+++ console/src/main/java/com/ctrip/framework/drc/console/config/WebMvcConfigSupplement.java
@@ -0,0 +1,43 @@
+package com.ctrip.framework.drc.console.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.*;
+
+
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * @ClassName WebMvcConfigSupplement
+ * @Author haodongPan
+ * @Date 2022/1/27 17:24
+ * @Version: $
+ */
+@Configuration
+public class WebMvcConfigSupplement extends WebMvcConfigurerAdapter {
+    private final Logger logger = Logger.getLogger(getClass().getSimpleName());
+    
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.extendMessageConverters(converters);
+        MappingJackson2HttpMessageConverter defaultConverter = null;
+        for (HttpMessageConverter converter : converters) {
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                defaultConverter = (MappingJackson2HttpMessageConverter) converter;
+                break;
+            }
+        }
+        if (defaultConverter != null) {
+            converters.remove(defaultConverter);
+            converters.set(0, defaultConverter);
+        }
+        logger.info("!!! default converter use MappingJackson2HttpMessageConverter");
+    }
+    
+}
