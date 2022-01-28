2ef515d845fec0619018efc0be53b62311a648b1
hdpan <hdpan@trip.com>
2022-01-28 15:20:58 +0800

    sso_upgrade_test

--- services/trip-service/src/main/java/com/ctrip/framework/drc/service/console/ServletConfig.java
+++ services/trip-service/src/main/java/com/ctrip/framework/drc/service/console/ServletConfig.java
@@ -17,21 +17,11 @@ public class ServletConfig {
     public ServletRegistrationBean logoutServletRegistration() {
 
         ServletRegistrationBean registration = new ServletRegistrationBean();
-//        for openSource use blankLogoutServlet
-//        registration.setServlet(new BlankLogoutServlet());        
         registration.setServlet(new Logout());
-
-        Map<String, String> initParamMap = new HashMap<>();
-        // 登出后跳转到的url，各环境会不一样，最好通过qconfig配置。
-        initParamMap.put("serverName", "http://localhost:8080");
-        registration.setInitParameters(initParamMap);
-
         Collection<String> urlMappings = new ArrayList<>();
         urlMappings.add("/logout");
         registration.setUrlMappings(urlMappings);
-
         registration.setLoadOnStartup(2);
-
         return registration;
     }
 }
