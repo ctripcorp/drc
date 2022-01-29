c5a1f2c0625b797c5ddd1e03b1453db804b6a2ef
hdpan <hdpan@trip.com>
2022-01-29 15:52:43 +0800

    sso_test

--- console/src/main/java/com/ctrip/framework/drc/console/config/WebMvcConfigSupplement.java
+++ console/src/main/java/com/ctrip/framework/drc/console/config/WebMvcConfigSupplement.java
@@ -1,6 +1,7 @@
 package com.ctrip.framework.drc.console.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.http.MediaType;
@@ -9,6 +10,11 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.Jackson2ObjectMapperFactoryBean;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.*;
 
 
@@ -22,38 +28,38 @@ import java.util.logging.Logger;
  * @Date 2022/1/27 17:24
  * @Version: $
  */
-@Configuration
-public class WebMvcConfigSupplement extends WebMvcConfigurerAdapter {
-    private final Logger logger = Logger.getLogger(getClass().getSimpleName());
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy-MM-dd"));
-        converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
-    }
-
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        super.extendMessageConverters(converters);
-        MappingJackson2HttpMessageConverter defaultConverter = null;
-        for (HttpMessageConverter converter : converters) {
-            if (converter instanceof MappingJackson2HttpMessageConverter) {
-                defaultConverter = (MappingJackson2HttpMessageConverter) converter;
-                break;
-            }
-        }
-        if (defaultConverter != null) {
-            converters.remove(defaultConverter);
-            converters.set(0, defaultConverter);
-            logger.info("!!! default converter use MappingJackson2HttpMessageConverter");
-        }
-        logger.info("!!! default converter no use MappingJackson2HttpMessageConverter");
-    }
-
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.defaultContentType(MediaType.APPLICATION_JSON);
-    }
-
-}
+//@Configuration
+//public class WebMvcConfigSupplement extends WebMvcConfigurerAdapter {
+//    private final Logger logger = Logger.getLogger(getClass().getSimpleName());
+//
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+//        builder.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+//        converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
+//    }
+//
+//    @Override
+//    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        super.extendMessageConverters(converters);
+//        MappingJackson2HttpMessageConverter defaultConverter = null;
+//        for (HttpMessageConverter converter : converters) {
+//            if (converter instanceof MappingJackson2HttpMessageConverter) {
+//                defaultConverter = (MappingJackson2HttpMessageConverter) converter;
+//                break;
+//            }
+//        }
+//        if (defaultConverter != null) {
+//            converters.remove(defaultConverter);
+//            converters.set(0, defaultConverter);
+//            logger.info("!!! default converter use MappingJackson2HttpMessageConverter");
+//        }
+//        logger.info("!!! default converter no use MappingJackson2HttpMessageConverter");
+//    }`
+//
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+//    }
+//
+//}
