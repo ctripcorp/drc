7d1afc6fbb32ce2195317d638be0f7a3cfea9af9
hdpan <hdpan@trip.com>
2022-02-15 16:44:20 +0800

    gtid moniotr period config in qconfig

--- console/src/main/java/com/ctrip/framework/drc/console/monitor/delay/config/MonitorTableSourceProvider.java
+++ console/src/main/java/com/ctrip/framework/drc/console/monitor/delay/config/MonitorTableSourceProvider.java
@@ -42,7 +42,7 @@ public class MonitorTableSourceProvider extends AbstractConfigBean {
 
     private static final String GTID_MONITOR_SWITCH = "gtid.monitor.switch";
     private static final String GTID_MONITOR_PERIOD = "gtid.monitor.period";
-    private static final int DEFAULT_GTID_MONITOR_PERIOD = 30;
+    private static final int DEFAULT_GTID_MONITOR_PERIOD = 120;
 
     public static final String DRC_DELAY_MESUREMENT = "fx.drc.delay";
 
