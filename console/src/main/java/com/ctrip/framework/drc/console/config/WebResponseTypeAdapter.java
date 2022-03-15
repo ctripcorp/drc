6efde5156df800f8a9c7d40d8edadc6e5308282d
hdpan <hdpan@trip.com>
2022-03-15 16:43:40 +0800

    http or qconfig open degrade

---
+++ console/src/main/java/com/ctrip/framework/drc/console/config/WebResponseTypeAdapter.java
@@ -0,0 +1,24 @@
+package com.ctrip.framework.drc.console.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+/**
+ * @ClassName WebConfig
+ * @Author haodongPan
+ * @Date 2022/3/15 14:50
+ * @Version: $
+ */
+@Configuration
+public class WebResponseTypeAdapter extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 从converters中移除xml的converter
+        converters.removeIf(converter -> converter instanceof MappingJackson2XmlHttpMessageConverter);
+    }
+}
