4066c9c4a64dfae917c4d45ff04f8fcd995aa185
hdpan <hdpan@trip.com>
2022-03-21 10:47:47 +0800

    ut

--- core/src/test/java/com/ctrip/framework/drc/core/service/ops/AppNodeTest.java
+++ core/src/test/java/com/ctrip/framework/drc/core/service/ops/AppNodeTest.java
@@ -12,12 +12,19 @@ public class AppNodeTest {
         AppNode appNode0 = new AppNode();
         appNode0.setIp("");
         appNode0.setPort(8080);
+        appNode0.setIdc("shaoy");
+        appNode0.getIdc();
+        
 
         AppNode appNode1 = new AppNode();
         appNode1.setIp("ip2");
         appNode1.setPort(8080);
+        appNode1.setIdc("sharb");
+        appNode1.toString();
 
+        Assert.assertTrue(appNode1.isLegal());
         Assert.assertFalse(appNode0.isLegal());
         Assert.assertFalse(appNode0.equals(appNode1));
+        Assert.assertFalse(appNode0.hashCode() == appNode1.hashCode());
     }
 }
