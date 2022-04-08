4d8a2b1f06542c3b9abbaa7ece7a237c9a028364
md_li <md_li@trip.com>
2022-04-08 20:36:46 +0800

    skip heartbeat for replicator

--- replicator/src/main/java/com/ctrip/framework/drc/replicator/impl/inbound/converter/ReplicatorByteBufConverter.java
+++ replicator/src/main/java/com/ctrip/framework/drc/replicator/impl/inbound/converter/ReplicatorByteBufConverter.java
@@ -70,9 +70,6 @@ public class ReplicatorByteBufConverter extends AbstractByteBufConverter {
             case drc_ddl_log_event:
                 logEvent =  new DrcDdlLogEvent();
                 return logEvent;
-            case drc_heartbeat_log_event:
-                logEvent =  new DrcHeartbeatLogEvent();
-                return logEvent;
             default:
                 return null;
         }
