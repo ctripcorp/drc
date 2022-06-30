42ca2af99daa77b237561c825ddf437fc3d75bc7
md_li <md_li@trip.com>
2022-06-30 12:01:24 +0800

    cache configed list

--- core/src/main/java/com/ctrip/framework/drc/core/server/common/filter/row/UidConfiguration.java
+++ core/src/main/java/com/ctrip/framework/drc/core/server/common/filter/row/UidConfiguration.java
@@ -1,5 +1,6 @@
 package com.ctrip.framework.drc.core.server.common.filter.row;
 
+import com.ctrip.framework.drc.core.monitor.util.IsolateHashCache;
 import com.ctrip.xpipe.config.AbstractConfigBean;
 
 import java.util.Set;
@@ -10,11 +11,15 @@ import java.util.Set;
  */
 public class UidConfiguration extends AbstractConfigBean {
 
-    private static final String UID_FILTER = "uid.filter";
+    private static final String UID_FILTER = "uid.filter.";
 
-    private static final String UID_WHITELIST = UID_FILTER + ".whitelist.%s";
+    private static final String UID_WHITELIST = UID_FILTER + "whitelist.%s";
 
-    private static final String UID_BLACKLIST = UID_FILTER + ".blacklist.%s";
+    private static final String UID_BLACKLIST = UID_FILTER + "blacklist.%s";
+
+    private IsolateHashCache<String, Set<String>> blackListCache = new IsolateHashCache<>(5000, 16, 4);
+
+    private IsolateHashCache<String, Set<String>> whiteListCache = new IsolateHashCache<>(5000, 16, 4);
 
     private UidConfiguration() {}
 
@@ -26,22 +31,20 @@ public class UidConfiguration extends AbstractConfigBean {
         return UidConfigurationHolder.INSTANCE;
     }
 
-    public boolean filterRowsWithBlackList(String uid, String registryKey) {
-        String blackList = String.format(UID_BLACKLIST, registryKey);
-        Set<String> blackListUids = getSplitStringSet(blackList);
-        if (blackListUids.contains(uid.trim())) {
-            return false;
-        }
-        return true;
+    public boolean filterRowsWithBlackList(String uid, String registryKey) throws Exception {
+        Set<String> blackListUids = blackListCache.get(registryKey, () -> {
+            String blackList = String.format(UID_BLACKLIST, registryKey);
+            return getSplitStringSet(blackList);
+        });
+        return !blackListUids.contains(uid.trim());
     }
 
-    public boolean filterRowsWithWhiteList(String uid, String registryKey) {
-        String whiteList = String.format(UID_WHITELIST, registryKey);
-        Set<String> whiteListUids = getSplitStringSet(whiteList);
-        if (whiteListUids.contains(uid.trim())) {
-            return true;
-        }
-        return false;
+    public boolean filterRowsWithWhiteList(String uid, String registryKey) throws Exception {
+        Set<String> whiteListUids = whiteListCache.get(registryKey, () -> {
+            String whiteList = String.format(UID_WHITELIST, registryKey);
+            return getSplitStringSet(whiteList);
+        });
+        return whiteListUids.contains(uid.trim());
     }
 
     @Override
@@ -49,7 +52,10 @@ public class UidConfiguration extends AbstractConfigBean {
         super.onChange(key, oldValue, newValue);
         logger.debug("[onChange] for key {}:{}:{}", key, oldValue, newValue);
         if (key != null && key.startsWith(UID_FILTER)) {
-
+            String registryKey = key.substring(UID_FILTER.length());
+            blackListCache.invalidate(registryKey);
+            whiteListCache.invalidate(registryKey);
+            logger.info("[Invalidate] for key {}", registryKey);
         }
     }
 
