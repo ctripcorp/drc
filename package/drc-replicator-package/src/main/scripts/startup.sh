e7c40e59cce129b77081b606c80c87a57db49f1c
jixinwang <jixinwang@trip.com>
2022-07-28 14:01:47 +0800

    change jemalloc prof config

--- package/drc-replicator-package/src/main/scripts/startup.sh
+++ package/drc-replicator-package/src/main/scripts/startup.sh
@@ -162,7 +162,7 @@ export JAVA_OPTS="$JAVA_OPTS -cp .:./META-INF/"
 echo $JAVA_OPTS
 
 export LD_PRELOAD=/usr/local/lib/libjemalloc.so
-export MALLOC_CONF=prof:true,lg_prof_interval:30,lg_prof_sample:17,prof_prefix:/mnt/mesos/sandbox/100023498/jeprof
+export MALLOC_CONF=prof:false,lg_prof_interval:30,lg_prof_sample:17,prof_prefix:/opt/logs/100023498/jeprof
 
 PATH_TO_JAR=$SERVICE_NAME".jar"
 SERVER_URL="http://localhost:$SERVER_PORT/health"
