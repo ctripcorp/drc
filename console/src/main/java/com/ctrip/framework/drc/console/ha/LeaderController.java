df56888f2aa1b9fcfefc36e27360463eaf133074
hdpan <hdpan@trip.com>
2022-08-09 11:12:01 +0800

    console ut

---
+++ console/src/main/java/com/ctrip/framework/drc/console/ha/LeaderController.java
@@ -0,0 +1,30 @@
+package com.ctrip.framework.drc.console.ha;
+
+
+import com.ctrip.framework.drc.core.http.ApiResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @ClassName LeaderController
+ * @Author haodongPan
+ * @Date 2022/6/27 14:53
+ * @Version: $
+ */
+@RestController
+@RequestMapping("/api/drc/v1/leader/")
+public class LeaderController {
+    
+    @Autowired
+    private ConsoleLeaderElector consoleLeaderElector;
+    
+    @GetMapping ("status")
+    public ApiResult status(){
+        return ApiResult.getSuccessInstance(consoleLeaderElector.amILeader() ? "leader" : "not leader");
+    }
+
+    
+}
