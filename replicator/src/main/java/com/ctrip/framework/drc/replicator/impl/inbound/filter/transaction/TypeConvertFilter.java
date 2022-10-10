a3efdc6709d41f222ae650bef6eb2b5834b18702
md_li <md_li@trip.com>
2022-10-10 23:57:54 +0800

    fix critical issue

--- replicator/src/main/java/com/ctrip/framework/drc/replicator/impl/inbound/filter/transaction/TypeConvertFilter.java
+++ replicator/src/main/java/com/ctrip/framework/drc/replicator/impl/inbound/filter/transaction/TypeConvertFilter.java
@@ -16,15 +16,17 @@ public class TypeConvertFilter extends AbstractTransactionFilter {
     @Override
     public boolean doFilter(ITransactionEvent transactionEvent) {
         List<LogEvent> logEvents = transactionEvent.getEvents();
-        int lastIndex = logEvents.size() - 1;
-        if (logEvents != null && (logEvents.get(lastIndex) instanceof TransactionTableMarkedXidLogEvent)) {
-            for (int i = lastIndex; i >= 0; i--) {
-                LogEvent logEvent = logEvents.get(i);
-                LogEventType logEventType = logEvent.getLogEventType();
-                if (LogEventType.gtid_log_event == logEventType) {
-                    GtidLogEvent gtidLogEvent = (GtidLogEvent) logEvent;
-                    gtidLogEvent.setEventType(LogEventType.drc_gtid_log_event.getType());
-                    break;
+        if (logEvents != null && !logEvents.isEmpty()) {
+            int lastIndex = logEvents.size() - 1;
+            if ((logEvents.get(lastIndex) instanceof TransactionTableMarkedXidLogEvent)) {
+                for (int i = lastIndex; i >= 0; i--) {
+                    LogEvent logEvent = logEvents.get(i);
+                    LogEventType logEventType = logEvent.getLogEventType();
+                    if (LogEventType.gtid_log_event == logEventType) {
+                        GtidLogEvent gtidLogEvent = (GtidLogEvent) logEvent;
+                        gtidLogEvent.setEventType(LogEventType.drc_gtid_log_event.getType());
+                        break;
+                    }
                 }
             }
         }
