9badf4c0fbca8b1d57f7adfc18200d94956fb0d5
jixinwang <jixinwang@trip.com>
2022-11-03 14:20:24 +0800

    integration-test add vi-server to consumer qmq-messenger

--- integration-test/src/main/java/com/ctrip/framework/drc/monitor/function/cases/messenger/MessengerConsumerCase.java
+++ integration-test/src/main/java/com/ctrip/framework/drc/monitor/function/cases/messenger/MessengerConsumerCase.java
@@ -2,9 +2,12 @@ package com.ctrip.framework.drc.monitor.function.cases.messenger;
 
 import com.ctrip.datasource.message.BinlogMessage;
 import com.ctrip.framework.dal.cluster.client.message.NonLocalBinlogConsumer;
+import com.ctrip.framework.vi.server.VIServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by jixinwang on 2022/11/2
@@ -13,6 +16,12 @@ import org.springframework.stereotype.Service;
 public class MessengerConsumerCase {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @PostConstruct
+    public void init() throws Exception {
+        VIServer viServer = new VIServer(9999);//Listener 模式需要启动VI;
+        viServer.start();
+    }
 
     private void manualCreateConsumer() {
 
