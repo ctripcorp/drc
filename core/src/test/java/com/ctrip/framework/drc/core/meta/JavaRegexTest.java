5a88e2b4ce70e45088f66d718868187638d7c1a5
jixinwang <jixinwang@trip.com>
2022-11-17 21:19:52 +0800

    change integration-test package

--- core/src/test/java/com/ctrip/framework/drc/core/meta/JavaRegexTest.java
+++ core/src/test/java/com/ctrip/framework/drc/core/meta/JavaRegexTest.java
@@ -3,6 +3,7 @@ package com.ctrip.framework.drc.core.meta;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -12,7 +13,13 @@ public class JavaRegexTest {
 
     @Test
     public void testRegex() {
-        boolean result = Pattern.matches(".*", "drc1.insert1");
+        Pattern pattern = Pattern.compile("(?i)^Trip$");
+        Matcher matcher =  pattern.matcher("trip");
+        boolean result = matcher.find();
         Assert.assertTrue(result);
+
+        Matcher matcher2 =  pattern.matcher("tripp");
+        boolean result2 = matcher2.find();
+        Assert.assertFalse(result2);
     }
 }
