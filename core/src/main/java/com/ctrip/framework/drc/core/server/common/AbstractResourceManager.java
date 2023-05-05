0f054b4c3996d97eb91766a3552c1db7f52f8720
jixinwang <jixinwang@trip.com>
2023-05-05 18:01:09 +0800

    keep retrying when registering Applier

--- core/src/main/java/com/ctrip/framework/drc/core/server/common/AbstractResourceManager.java
+++ core/src/main/java/com/ctrip/framework/drc/core/server/common/AbstractResourceManager.java
@@ -3,6 +3,7 @@ package com.ctrip.framework.drc.core.server.common;
 import com.ctrip.framework.drc.core.driver.config.InstanceStatus;
 import com.ctrip.framework.drc.core.server.container.ZookeeperValue;
 import com.ctrip.framework.drc.core.server.utils.IpUtils;
+import com.ctrip.framework.drc.core.server.utils.ThreadUtils;
 import com.ctrip.framework.drc.core.server.zookeeper.DrcZkConfig;
 import com.ctrip.xpipe.api.cluster.LeaderElector;
 import com.ctrip.xpipe.api.cluster.LeaderElectorManager;
@@ -17,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author limingdong
@@ -27,6 +30,10 @@ public abstract class AbstractResourceManager implements Releasable {
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
     protected Map<String, LeaderElector> zkLeaderElectors = Maps.newConcurrentMap();
+
+    private final static int RESTART_DELAY_TIME = 5;
+
+    private ScheduledExecutorService scheduledExecutorService = ThreadUtils.newSingleThreadScheduledExecutor(getClass().getSimpleName());
 
     @Autowired
     private DrcZkConfig drcZkConfig;
@@ -50,15 +57,39 @@ public abstract class AbstractResourceManager implements Releasable {
                 leaderElector.initialize();
                 leaderElector.start();
             } catch (Exception e) {
-                logger.error("LeaderElector error for {}", registryKey, e);
+                logger.error("LeaderElector error for {}, lifecycle state is {}", registryKey, leaderElector.getLifecycleState(), e);
+                scheduleRestart(leaderElector, registryKey);
             }
         }
         return leaderElector;
     }
 
+    private void scheduleRestart(LeaderElector leaderElector, String registryKey) {
+        if (leaderElector.getLifecycleState().isInitialized()) {
+            scheduledExecutorService.schedule(() -> doRestart(leaderElector, registryKey), RESTART_DELAY_TIME, TimeUnit.SECONDS);
+        }
+    }
+
+    private void doRestart(LeaderElector leaderElector, String registryKey) {
+        try {
+            if (leaderElector.getLifecycleState().isInitialized()) {
+                logger.info("LeaderElector restart for {}", registryKey);
+                leaderElector.start();
+                if (leaderElector != zkLeaderElectors.get(registryKey)) {
+                    leaderElector.stop();
+                    leaderElector.dispose();
+                }
+            }
+        } catch (Exception e) {
+            logger.error("LeaderElector error when restart for {}", registryKey, e);
+            scheduleRestart(leaderElector, registryKey);
+        }
+    }
+
     @Override
     public void release() {
         logger.info("run AbstractResourceManager resources release");
+        scheduledExecutorService.shutdown();
         for (Lifecycle lifecycle : zkLeaderElectors.values()) {
             try {
                 lifecycle.stop();
