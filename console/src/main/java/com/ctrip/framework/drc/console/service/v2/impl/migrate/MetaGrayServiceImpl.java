d78392662294798d57251e1106310aaf5669be30
hdpan <hdpan@trip.com>
2023-07-20 17:51:56 +0800

    compare xml schedule and fix npe in query MhaUuid when syncMhaInfo from dal

--- console/src/main/java/com/ctrip/framework/drc/console/service/v2/impl/migrate/MetaGrayServiceImpl.java
+++ console/src/main/java/com/ctrip/framework/drc/console/service/v2/impl/migrate/MetaGrayServiceImpl.java
@@ -5,29 +5,18 @@ import com.ctrip.framework.drc.console.config.MhaGrayConfig;
 import com.ctrip.framework.drc.console.monitor.AbstractMonitor;
 import com.ctrip.framework.drc.console.monitor.delay.config.DbClusterSourceProvider;
 import com.ctrip.framework.drc.console.monitor.delay.config.v2.MetaProviderV2;
-import com.ctrip.framework.drc.console.service.DrcBuildService;
+import com.ctrip.framework.drc.console.service.v2.MetaCompareService;
 import com.ctrip.framework.drc.console.service.v2.MetaGrayService;
 import com.ctrip.framework.drc.core.entity.DbCluster;
 import com.ctrip.framework.drc.core.entity.Dc;
 import com.ctrip.framework.drc.core.entity.Drc;
-import com.ctrip.framework.drc.core.server.utils.ThreadUtils;
 import com.ctrip.framework.drc.core.transform.DefaultSaxParser;
-import com.google.common.collect.Lists;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 /**
  * @ClassName MetaGrayServiceImpl
@@ -44,16 +33,12 @@ public class MetaGrayServiceImpl extends AbstractMonitor implements MetaGrayServ
 
     @Autowired private DbClusterSourceProvider metaProviderV1;
 
-    @Autowired private DrcBuildService drcBuildService;
-
     @Autowired private DefaultConsoleConfig consoleConfig;
 
     @Autowired private MhaGrayConfig mhaGrayConfig;
-
-    private StringBuilder recorder;
-    private final ExecutorService comparators = ThreadUtils.newCachedThreadPool("metaCompare");
-    private final ReentrantLock lock = new ReentrantLock();
-
+    
+    @Autowired private MetaCompareService metaCompareService;
+    
     private volatile Drc grayDrc;
 
 
@@ -63,6 +48,24 @@ public class MetaGrayServiceImpl extends AbstractMonitor implements MetaGrayServ
         Drc drcWithOneDc = new Drc();
         drcWithOneDc.addDc(dc);
         return drcWithOneDc;
+    }
+
+    @Override
+    public Drc getDrc()  {
+        if (null == grayDrc) {
+            scheduledTask();
+        }
+        return grayDrc;
+    }
+
+    @Override
+    public DbClusterCompareRes compareDbCluster(String dbClusterId) {
+        return metaCompareService.compareDbCluster(dbClusterId);
+    }
+
+    @Override
+    public String compareDrcMeta() throws Exception {
+        return metaCompareService.compareDrcMeta();
     }
 
     @Override
@@ -85,7 +88,7 @@ public class MetaGrayServiceImpl extends AbstractMonitor implements MetaGrayServ
         }
     }
 
-    public synchronized Drc refreshDrc()  {
+    protected synchronized Drc refreshDrc()  {
         Drc oldDrc = metaProviderV1.getDrc();
         Set<String> publicCloudRegion = consoleConfig.getPublicCloudRegion();
         String localRegion = consoleConfig.getRegion();
@@ -93,7 +96,7 @@ public class MetaGrayServiceImpl extends AbstractMonitor implements MetaGrayServ
             return oldDrc;
         }
         try {
-            if (mhaGrayConfig.getDbClusterGraySwitch()) {
+            if (mhaGrayConfig.getDbClusterGraySwitch()  && metaCompareService.isConsistent()) {
                 Drc drcCopy = DefaultSaxParser.parse(oldDrc.toString());
                 int memoryAddress1 = System.identityHashCode(oldDrc);
                 int memoryAddress2 = System.identityHashCode(drcCopy);
@@ -102,153 +105,21 @@ public class MetaGrayServiceImpl extends AbstractMonitor implements MetaGrayServ
                 for (String dbClusterId : mhaGrayConfig.getGrayDbClusterSet()) {
                     Dc newDc = metaProviderV2.getDcBy(dbClusterId);
                     DbCluster newDcDbCluster = newDc.findDbCluster(dbClusterId);
-
-                    if(!mhaGrayConfig.getDbClusterGrayCompareSwitch()  || metaConsistent(dbClusterId)) {
-                        Dc dcCopy = drcCopy.findDc(newDc.getId());
-                        logger.info("[[tag=metaGray]] gray dbClusterId:{},oldDbCluster:{},newDbCluster:{}",dbClusterId,
-                                dcCopy.findDbCluster(dbClusterId),newDcDbCluster);
-                        dcCopy.removeDbCluster(dbClusterId);
-                        dcCopy.addDbCluster(newDcDbCluster);
-                    } else {
-                        logger.error("[[tag=metaGray]] dbClusterId:{} xml not equals ,do nothing", dbClusterId);
-                    }
+                    
+                    Dc dcCopy = drcCopy.findDc(newDc.getId());
+                    logger.info("[[tag=metaGray]] gray dbClusterId:{},oldDbCluster:{},newDbCluster:{}",dbClusterId,
+                            dcCopy.findDbCluster(dbClusterId),newDcDbCluster);
+                    dcCopy.removeDbCluster(dbClusterId);
+                    dcCopy.addDbCluster(newDcDbCluster);
                 }
                 return drcCopy;
+            } else {
+                logger.info("[[tag=metaGray]] use old meta");
             }
         } catch (Throwable e) {
             logger.error("[[tag=metaGray]] gray new meta fail,use old meta", e);
         }
         return oldDrc;
     }
-
-    @Override
-    public Drc getDrc()  {
-        if (null == grayDrc) {
-            grayDrc = refreshDrc();
-        }
-        return grayDrc;
-    }
-
-
-    @Override
-    public DbClusterCompareRes compareDbCluster(String dbClusterId) {
-        DbClusterCompareRes res = new DbClusterCompareRes();
-        try {
-            DbCluster oldDbCluster =  metaProviderV1.getDcBy(dbClusterId).findDbCluster(dbClusterId);
-            DbCluster newDbCluster =  metaProviderV2.getDcBy(dbClusterId).findDbCluster(dbClusterId);
-            res.setOldDbCluster(oldDbCluster);
-            res.setNewDbCluster(newDbCluster);
-            String compareRes = new DbClusterComparator(
-                    oldDbCluster, newDbCluster, drcBuildService,
-                    consoleConfig.getCostTimeTraceSwitch(),consoleConfig.getLocalConfigCloudDc()).call();
-
-            res.setCompareRes(compareRes);
-        } catch (Exception e) {
-            logger.error("[[tag=metaCompare]] compare dbCluster fail:{}",dbClusterId,e);
-            res.setCompareRes("compare fail");
-        }
-        return res;
-    }
-
-    private boolean metaConsistent(String dbClusterId) {
-        DbClusterCompareRes res = compareDbCluster(dbClusterId);
-        String compareRes = res.getCompareRes();
-        return !(compareRes.contains("not equal") || compareRes.contains("empty") || compareRes.contains("fail"));
-    }
-
-    @Override
-    public String compareDrcMeta()  {
-        try {
-            lock.lock();
-            Drc newDrc = metaProviderV2.getDrc();
-            Drc oldDrc = metaProviderV1.getDrc();
-            recorder = new StringBuilder();
-            compareLogically(oldDrc, newDrc);
-        } finally {
-            lock.unlock();
-            logger.warn("[[tag=metaCompare]] res:{}",recorder.toString());
-        }
-        return recorder.toString();
-    }
-
-
-    protected void compareLogically(Drc oldDrc,Drc newDrc) {
-        Map<String, Dc> oldDcs = oldDrc.getDcs();
-        Map<String, Dc> newDcs = newDrc.getDcs();
-        if (oldDcs.size() != newDcs.size()) {
-            recorder.append("MetaDcs size is not equal!");
-        }
-        for (Entry<String, Dc> dcEntry : oldDcs.entrySet()) {
-            String dcId = dcEntry.getKey();
-            recorder.append("\n[CompareDc]:").append(dcId);
-
-            Dc oldDc = dcEntry.getValue();
-            Dc newDc = newDcs.getOrDefault(dcId, null);
-            if (null == newDc) {
-                recorder.append("\nnewMetaDc is empty!");
-                continue;
-            }
-
-            if (!oldDc.getRoutes().equals(newDc.getRoutes())) {
-                recorder.append("\nRoute is not equal!");
-            }
-            if (!oldDc.getClusterManagers().equals(newDc.getClusterManagers())) {
-                recorder.append("\nClusterManagers is not equal!");
-            }
-            if (!oldDc.getZkServer().equals(newDc.getZkServer())) {
-                recorder.append("\nZkServer is not equal!");
-            }
-
-            Map<String, DbCluster> oldDbClusters = oldDc.getDbClusters();
-            Map<String, DbCluster> newDbClusters = newDc.getDbClusters();
-            if (oldDbClusters.size() != newDbClusters.size()) {
-                recorder.append("\nDbCluster size is not equal!");
-            }
-
-            List<Future<String>> recorderFutures = Lists.newArrayList();
-
-            for (Entry<String, DbCluster> dbClusterEntry : oldDbClusters.entrySet()) {
-                String dbClusterId = dbClusterEntry.getKey();
-
-                DbCluster oldDbCluster = dbClusterEntry.getValue();
-                DbCluster newDbCluster = newDbClusters.getOrDefault(dbClusterId, null);
-
-                recorderFutures.add(comparators.submit(
-                        new DbClusterComparator(
-                                oldDbCluster, newDbCluster, drcBuildService, consoleConfig.getCostTimeTraceSwitch(),
-                                consoleConfig.getLocalConfigCloudDc()
-                        )));
-
-                if (recorderFutures.size() >= consoleConfig.getMetaCompareParallel()) {
-                    Iterator<Future<String>> iterator = recorderFutures.iterator();
-                    while (iterator.hasNext()) {
-                        Future<String> dbClusterRes = iterator.next();
-                        try {
-                            recorder.append(dbClusterRes.get());
-                            iterator.remove();
-                        } catch (InterruptedException | ExecutionException e) {
-                            logger.error("[[tag=xmlCompare]] compare DbCluster fail in parallel", e);
-                            recorder.append("compare DbCluster fail in parallel before:").append(oldDbCluster.getId());
-                        }
-
-                    }
-                }
-
-            }
-
-            // rest
-            if (!CollectionUtils.isEmpty(recorderFutures)) {
-                for (Future<String> recorderFuture : recorderFutures) {
-                    try {
-                        recorder.append(recorderFuture.get());
-                    } catch (Exception e) {
-                        logger.error("[[tag=xmlCompare]] compare DbCluster fail in parallel", e);
-                        recorder.append("compare DbCluster fail in parallel");
-                    }
-                }
-            }
-        }
-    }
-
 
 }
--- console/src/test/java/com/ctrip/framework/drc/console/service/v2/impl/migrate/MetaGrayServiceImplTest.java
+++ console/src/test/java/com/ctrip/framework/drc/console/service/v2/impl/migrate/MetaGrayServiceImplTest.java
@@ -1,19 +1,16 @@
 package com.ctrip.framework.drc.console.service.v2.impl.migrate;
 
-
 import com.ctrip.framework.drc.console.config.DefaultConsoleConfig;
 import com.ctrip.framework.drc.console.config.MhaGrayConfig;
 import com.ctrip.framework.drc.console.monitor.delay.config.DbClusterSourceProvider;
 import com.ctrip.framework.drc.console.monitor.delay.config.v2.MetaProviderV2;
-import com.ctrip.framework.drc.console.service.DrcBuildService;
-import com.ctrip.framework.drc.core.entity.Dc;
+import com.ctrip.framework.drc.console.service.v2.MetaCompareService;
 import com.ctrip.framework.drc.core.entity.Drc;
 import com.ctrip.framework.drc.core.transform.DefaultSaxParser;
 import com.ctrip.xpipe.utils.FileUtils;
 import com.google.common.collect.Sets;
 import java.io.IOException;
 import java.io.InputStream;
-import org.assertj.core.util.Lists;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,11 +28,11 @@ public class MetaGrayServiceImplTest {
 
     @Mock private DbClusterSourceProvider metaProviderV1;
 
-    @Mock private DrcBuildService drcBuildService;
-
     @Mock private DefaultConsoleConfig consoleConfig;
     
     @Mock private MhaGrayConfig mhaGrayConfig;
+
+    @Mock private MetaCompareService metaCompareService;
 
     private Drc oldDrc;
     private Drc newDrc;
@@ -52,54 +49,11 @@ public class MetaGrayServiceImplTest {
         
         Mockito.when(metaProviderV1.getDrc()).thenReturn(oldDrc);
         Mockito.when(metaProviderV2.getDrc()).thenReturn(newDrc);
-
-
-        // for old meta
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha1"),Mockito.eq("db1\\.t2,db2\\.t2")))
-                .thenReturn(Lists.newArrayList("db1.t2","db2.t2"));
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha3"),Mockito.eq("nameMappingshard0[1-2]db\\.t[1-8]")))
-                .thenReturn(Lists.newArrayList("nameMappingshard01db.t1","nameMappingshard02db.t1"));
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha2"),Mockito.eq("nameMappingshard0[1-2]db\\.s[1-8],rowsFiltersharddb[1-2]\\.s[1-8],columnsFiltershardb[1-2]\\..*")))
-                .thenReturn(Lists.newArrayList("nameMappingshard01db.s1","nameMappingshard02db.s1","rowsFiltersharddb1.s1","rowsFiltersharddb2.s1","columnsFiltershardb1.s1","columnsFiltershardb2.s1"));
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha2"),Mockito.eq("rowsFiltersharddb[1-2]\\.s[1-8]")))
-                .thenReturn(Lists.newArrayList("rowsFiltersharddb1.s1","rowsFiltersharddb2.s1"));
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha2"),Mockito.eq("columnsFiltershardb[1-2]\\..*")))
-                .thenReturn(Lists.newArrayList("columnsFiltershardb1.s1","columnsFiltershardb2.s1"));
+        Mockito.when(metaCompareService.isConsistent()).thenReturn(true);
         
-        // for new meta
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha1"),Mockito.eq("db1\\.t2,db2\\.t2")))
-                .thenReturn(Lists.newArrayList("db1.t2","db2.t2"));
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha3"),Mockito.eq("nameMappingshard01db\\.t[1-8],nameMappingshard02db\\.t[1-8]")))
-                .thenReturn(Lists.newArrayList("nameMappingshard01db.t1","nameMappingshard02db.t1"));
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha2"),Mockito.eq("nameMappingshard01db\\.s[1-8],nameMappingshard02db\\.s[1-8],rowsFiltersharddb1\\.s[1-8],rowsFiltersharddb2\\.s[1-8],columnsFiltershardb1\\..*,columnsFiltershardb2\\..*")))
-                .thenReturn(Lists.newArrayList("nameMappingshard01db.s1","nameMappingshard02db.s1","rowsFiltersharddb1.s1","rowsFiltersharddb2.s1","columnsFiltershardb1.s1","columnsFiltershardb2.s1"));
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha2"),Mockito.eq("rowsFiltersharddb1\\.s[1-8],rowsFiltersharddb2\\.s[1-8]")))
-                .thenReturn(Lists.newArrayList("rowsFiltersharddb1.s1","rowsFiltersharddb2.s1"));
-        Mockito.when(drcBuildService.queryTablesWithNameFilter(Mockito.eq("mha2"),Mockito.eq("columnsFiltershardb1\\..*,columnsFiltershardb2\\..*")))
-                .thenReturn(Lists.newArrayList("columnsFiltershardb1.s1","columnsFiltershardb2.s1"));
-        
-        
-        Mockito.when(consoleConfig.getMetaCompareParallel()).thenReturn(1);
     }
-
-  
-
-    @Test
-    public void testCompareLogically() {
-        String compareRecorder = metaGrayService.compareDrcMeta();
-        boolean equals = !(compareRecorder.contains("not equal") || compareRecorder.contains("empty"));
-        Assert.assertTrue(equals);
-    }
-
-    @Test
-    public void testCompareDbCluster() {
-        Mockito.when(metaProviderV1.getDcBy(Mockito.eq("mha3_dalcluster.mha3"))).thenReturn(oldDrc.findDc("ntgxy"));
-        Mockito.when(metaProviderV2.getDcBy(Mockito.eq("mha3_dalcluster.mha3"))).thenReturn(newDrc.findDc("ntgxy"));
-        
-        DbClusterCompareRes dbClusterCompareRes = metaGrayService.compareDbCluster("mha3_dalcluster.mha3");
-        String compareRes = dbClusterCompareRes.getCompareRes();
-        Assert.assertTrue((!compareRes.contains("not equal")) && (!compareRes.contains("empty")) && (!compareRes.contains("fail")));
-    }
+    
+    
 
     @Test
     public void testGetDrcInGrayMode() throws IOException, SAXException {
