9c9f51357c86539206b63e8c6a340beea1f9164e
yongnian <yongnianzheng@trip.com>
2023-07-28 11:00:38 +0800

    v2 view controller

--- console/src/main/java/com/ctrip/framework/drc/console/param/v2/MhaQuery.java
+++ console/src/main/java/com/ctrip/framework/drc/console/param/v2/MhaQuery.java
@@ -1,7 +1,7 @@
 package com.ctrip.framework.drc.console.param.v2;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
