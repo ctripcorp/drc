248fa68c2ba20a762308db6aa6161a0862551935
yongnian <yongnianzheng@trip.com>
2023-08-03 19:35:43 +0800

    commit

--- console/src/test/java/com/ctrip/framework/drc/console/controller/v2/MhaReplicationControllerTest.java
+++ console/src/test/java/com/ctrip/framework/drc/console/controller/v2/MhaReplicationControllerTest.java
@@ -39,11 +39,9 @@ import java.util.stream.Collectors;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-//@WebMvcTest(controllers = MhaReplicationController.class)
 @RunWith(MockitoJUnitRunner.class)
 public class MhaReplicationControllerTest {
     protected Logger logger = LoggerFactory.getLogger(getClass());
-
 
     private MockMvc mvc;
 
@@ -82,7 +80,7 @@ public class MhaReplicationControllerTest {
         MhaReplicationQuery query = new MhaReplicationQuery();
         Mockito.when(mhaReplicationServiceV2.queryByPage(query)).thenReturn(PageResult.newInstance(mhaReplicationTbls, 1, 10, 2));
 
-        Mockito.when(mhaServiceV2.query("mha3",null,null)).thenReturn(Collections.emptyMap());
+        Mockito.when(mhaServiceV2.query("mha3", null, null)).thenReturn(Collections.emptyMap());
 
     }
 
@@ -95,8 +93,7 @@ public class MhaReplicationControllerTest {
                 .andReturn();
         ApiResult<List<MhaReplicationVo>> apiResult = JSON.parseObject(relatedMhaId.getResponse().getContentAsString(), ApiResult.class);
         Assert.assertNull(apiResult.getData());
-        Assert.assertEquals(1L, apiResult.getStatus().longValue());
-        Assert.assertTrue(apiResult.getMessage().contains("请求参数异常"));
+        Assert.assertEquals(ResultCode.HANDLE_FAIL.getCode(), apiResult.getStatus().longValue());
     }
 
     @Test
@@ -130,8 +127,8 @@ public class MhaReplicationControllerTest {
     @Test
     public void testQueryPageEmptyInput() throws Exception {
         MvcResult relatedMhaId = mvc.perform(MockMvcRequestBuilders.get("/api/drc/v2/replication/query")
-                        .param("pageIndex","1")
-                        .param("pageSize","20")
+                        .param("pageIndex", "1")
+                        .param("pageSize", "20")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
@@ -145,9 +142,9 @@ public class MhaReplicationControllerTest {
     @Test
     public void testQueryPageNormalInput() throws Exception {
         MvcResult relatedMhaId = mvc.perform(MockMvcRequestBuilders.get("/api/drc/v2/replication/query")
-                        .param("pageIndex","1")
-                        .param("pageSize","20")
-                        .param("srcMha.name","mha3")
+                        .param("pageIndex", "1")
+                        .param("pageSize", "20")
+                        .param("srcMha.name", "mha3")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
