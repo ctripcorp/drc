a2e9dfb7dbe78d531338f1b530a1abd1ca789b17
yongnian <yongnianzheng@trip.com>
2023-08-03 19:21:27 +0800

    remove chinese

--- console/src/main/java/com/ctrip/framework/drc/console/enums/ReadableErrorDefEnum.java
+++ console/src/main/java/com/ctrip/framework/drc/console/enums/ReadableErrorDefEnum.java
@@ -7,19 +7,19 @@ package com.ctrip.framework.drc.console.enums;
  */
 public enum ReadableErrorDefEnum implements IErrorDef {
 
-    REQUEST_PARAM_INVALID("REQUEST_PARAM_INVALID", "请求参数异常"),
+    REQUEST_PARAM_INVALID("REQUEST_PARAM_INVALID", "invalid param! "),
 
     /**
      * 查询正常，结果为空
      */
-    QUERY_RESULT_EMPTY("QUERY_RESULT_EMPTY", "查询结果为空！"),
-    QUERY_DATA_INCOMPLETE("QUERY_DATA_INCOMPLETE", "查询异常，请联系开发！"),
+    QUERY_RESULT_EMPTY("QUERY_RESULT_EMPTY", "query result is empty"),
+    QUERY_DATA_INCOMPLETE("QUERY_DATA_INCOMPLETE", "query exception, please contact devops"),
 
 
     /**
      * 查询数据库失败
      */
-    QUERY_TBL_EXCEPTION("QUERY_TBL_EXCEPTION", "查询数据失败，请联系开发！");
+    QUERY_TBL_EXCEPTION("QUERY_TBL_EXCEPTION", "query datasource exception, please contact devops");
 
     ReadableErrorDefEnum(String code, String message) {
         this.code = code;
