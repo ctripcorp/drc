683b12d2edac726dadb2e52664c32cc24dd3720d
dengquanliang <ql_deng@trip.com>
2023-08-18 17:12:56 +0800

    impo rt

--- console/src/main/java/com/ctrip/framework/drc/console/controller/v2/ResourceMigrateController.java
+++ console/src/main/java/com/ctrip/framework/drc/console/controller/v2/ResourceMigrateController.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import qunar.api.open.annotation.RequestParam;
 
 import java.util.List;
 
