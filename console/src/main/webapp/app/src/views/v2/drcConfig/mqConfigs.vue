08456a776e1d40f2e931cc948d4491fa484860ea
yongnian <yongnianzheng@trip.com>
2023-09-21 17:54:14 +0800

    clean code

--- console/src/main/webapp/app/src/views/v2/drcConfig/mqConfigs.vue
+++ console/src/main/webapp/app/src/views/v2/drcConfig/mqConfigs.vue
@@ -2,7 +2,15 @@
   <base-component>
     <Breadcrumb :style="{margin: '15px 0 15px 185px', position: 'fixed'}">
       <BreadcrumbItem to="/home">首页</BreadcrumbItem>
-      <BreadcrumbItem to="/v2/mqConfigs">Mq投递配置 v2</BreadcrumbItem>
+      <BreadcrumbItem to="/v2/messengersV2">Messenger 集群</BreadcrumbItem>
+      <BreadcrumbItem :to="{
+          path: 'buildMessengerV2',query :{
+          step: 3,
+          mhaName: drc.mhaName
+        }
+      }">Messenger 配置
+      </BreadcrumbItem>
+      <BreadcrumbItem>Mq 同步表</BreadcrumbItem>
     </Breadcrumb>
     <Content class="content" :style="{padding: '10px', background: '#fff', margin: '50px 0 1px 185px', zIndex: '1'}">
       <Row>
--- console/src/main/webapp/app/src/views/v2/messengersV2.vue
+++ console/src/main/webapp/app/src/views/v2/messengersV2.vue
@@ -2,7 +2,7 @@
   <base-component>
     <Breadcrumb :style="{margin: '15px 0 15px 185px', position: 'fixed'}">
       <BreadcrumbItem to="/home">首页</BreadcrumbItem>
-      <BreadcrumbItem to="/v2/messengersV2">MQ投递集群</BreadcrumbItem>
+      <BreadcrumbItem >Messenger 集群</BreadcrumbItem>
     </Breadcrumb>
     <Content class="content" :style="{padding: '10px', background: '#fff', margin: '50px 0 1px 185px', zIndex: '1'}">
       <div style="padding: 1px 1px">
