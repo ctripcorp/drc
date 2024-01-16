ce79523f3298abbaafd68294cddc4db9ea8ee9f4
hdpan <hdpan@trip.com>
2024-01-16 15:12:52 +0800

    add test log

--- console/src/main/java/com/ctrip/framework/drc/console/monitor/delay/server/StaticDelayMonitorServer.java
+++ console/src/main/java/com/ctrip/framework/drc/console/monitor/delay/server/StaticDelayMonitorServer.java
@@ -376,7 +376,7 @@ public class StaticDelayMonitorServer extends AbstractMySQLSlave implements MySQ
                         String mhaName = entry.getKey();
                         if (!mhasRelated.contains(mhaName)) {
                             if (isReplicatorMaster) {
-                                logger.info("check delay loss {}->{}",mhaName,config.getDestMha());
+                                logger.info("remove check delay loss {}->{}",mhaName,config.getDestMha());
                                 iterator.remove();
                             }
                             continue;
