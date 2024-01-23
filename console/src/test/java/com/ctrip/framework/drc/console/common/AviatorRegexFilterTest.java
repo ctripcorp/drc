f0f82b851372bf09ff795b64d2c6ec8cba416bd9
ql_deng <ql_deng@trip.com>
2024-01-23 19:30:27 +0800

    test
    Change-Id: Ic3b83760401c7c153aac507983ddc5f9968f1b7f

--- console/src/test/java/com/ctrip/framework/drc/console/common/AviatorRegexFilterTest.java
+++ console/src/test/java/com/ctrip/framework/drc/console/common/AviatorRegexFilterTest.java
@@ -4,7 +4,9 @@ import com.ctrip.framework.drc.core.server.common.filter.table.aviator.AviatorRe
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import org.junit.Test;
+import org.springframework.util.StopWatch;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,5 +26,89 @@ public class AviatorRegexFilterTest {
     public void testString() {
         List<String> list = Lists.newArrayList("a", "b", "c");
         System.out.println(Joiner.on("|").join(list));
+    }
+
+    @Test
+    public void testDbBlacklist() {
+        StringBuilder tableName = new StringBuilder("(table");
+        for (int i = 1; i <= 20; i++) {
+            tableName.append("|table" + i);
+        }
+        tableName.append(")");
+        List<String> blacklist = new ArrayList<>();
+        for (int i = 1; i <= 200; i++) {
+            blacklist.add("db" + i + "\\." + tableName);
+        }
+
+        String fullName = "db200.table20";
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i<= 10000; i++) {
+            AviatorRegexFilter filter = new AviatorRegexFilter(Joiner.on(",").join(blacklist));
+            filter.filter(fullName);
+        }
+        long endTime1 = System.currentTimeMillis();
+
+        for (int i = 0; i<= 10000; i++) {
+            filter(fullName, blacklist);
+        }
+
+        long endTime2 = System.currentTimeMillis();
+
+        System.out.println(" cost: " + (endTime1 - startTime));
+        System.out.println(" cost: " + (endTime2 - endTime1));
+    }
+
+    @Test
+    public void testDbBlacklist1() {
+        StringBuilder tableName = new StringBuilder("(table");
+        for (int i = 1; i <= 200; i++) {
+            tableName.append("|table" + i);
+        }
+        tableName.append(")");
+        List<String> blacklist = new ArrayList<>();
+        for (int i = 1; i <= 10000; i++) {
+            blacklist.add("db" + i + "\\." + tableName);
+        }
+
+        String fullName = "db10000.table1900";
+
+        long startTime = System.currentTimeMillis();
+        boolean filter = filter(fullName, blacklist);
+        long endTime = System.currentTimeMillis();
+
+        long startTime1 = System.currentTimeMillis();
+        boolean filter1 = filter1(fullName, blacklist);
+        long endTime1 = System.currentTimeMillis();
+
+        System.out.println("res: " + filter + " cost: " + (endTime - startTime));
+        System.out.println("res1: " + filter1 + " cost: " + (endTime1 - startTime1));
+    }
+
+    private boolean filter(String fullName, List<String> blacklist) {
+        int size = blacklist.size();
+        List<String> filterBlacklist = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            filterBlacklist.add(blacklist.get(i));
+            if (i % 5 == 0 || i == size - 1) {
+                AviatorRegexFilter filter = new AviatorRegexFilter(Joiner.on(",").join(filterBlacklist));
+                if (filter.filter(fullName)) {
+                    return true;
+                }
+                filterBlacklist = new ArrayList<>();
+            }
+        }
+        return false;
+    }
+
+    private boolean filter1(String fullName, List<String> blacklist) {
+        int size = blacklist.size();
+        for (int i = 0; i < size; i++) {
+            AviatorRegexFilter filter = new AviatorRegexFilter(blacklist.get(i));
+            if (filter.filter(fullName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
