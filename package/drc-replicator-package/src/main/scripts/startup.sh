859f973ed8c96a5699d290d5ec0507a2f3a5b6d5
shirx <shirx@trip.com>
2025-08-11 10:41:42 +0800

    cft replicator jvm size adjust to 10GB

--- package/drc-replicator-package/src/main/scripts/startup.sh
+++ package/drc-replicator-package/src/main/scripts/startup.sh
@@ -72,7 +72,7 @@ function getSafeXmx() {
     total=`getTotalMem`
     SAFE_PERCENT=70
     MAX_MEM=20
-    if [ $IDC = "SIN-AWS" ] || [ $IDC = "FRA-AWS" ] || [ $IDC = "SHA-ALI" ] || [ $IDC = "SGP-ALI" ];then
+    if [ $IDC = "SIN-AWS" ] || [ $IDC = "FRA-AWS" ] || [ $IDC = "SHA-ALI" ] || [ $IDC = "SGP-ALI" ] || [ $IDC = "CFTRB" ] || [ $IDC = "CFTXY" ];then
         MAX_MEM=10
     fi
     result=`expr $total \* $SAFE_PERCENT / 100`
